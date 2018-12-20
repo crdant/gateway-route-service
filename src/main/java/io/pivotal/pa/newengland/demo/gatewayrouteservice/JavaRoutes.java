039fe95dfb340994966bb5fb1c527cd105d5e916
Chuck D'Antonio <c_dantonio@acm.org>
2018-12-20 11:36:58 -0500

    Add route from Java

--- src/main/java/io/pivotal/pa/newengland/demo/gatewayrouteservice/JavaRoutes.java
+++ src/main/java/io/pivotal/pa/newengland/demo/gatewayrouteservice/JavaRoutes.java
@@ -1,4 +1,24 @@
 package io.pivotal.pa.newengland.demo.gatewayrouteservice;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class JavaRoutes {
+
+    @Bean("JavaRoutes")
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r ->
+                        r.path("/java/**")
+                                .filters(
+                                        f -> f.stripPrefix(1)
+                                )
+                                .uri("http://httpbin.org:80")
+                )
+                .build();
+    }
+
 }
