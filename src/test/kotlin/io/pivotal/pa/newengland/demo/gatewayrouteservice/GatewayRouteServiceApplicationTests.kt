620361d16e6e2d4c804c2e86da0a0e73d7191c2d
Chuck D'Antonio <c_dantonio@acm.org>
2018-12-20 11:27:17 -0500

    Initial commit with a Kotlin route

---
+++ src/test/kotlin/io/pivotal/pa/newengland/demo/gatewayrouteservice/GatewayRouteServiceApplicationTests.kt
@@ -0,0 +1,17 @@
+package io.pivotal.pa.newengland.demo.gatewayrouteservice
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class GatewayRouteServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
