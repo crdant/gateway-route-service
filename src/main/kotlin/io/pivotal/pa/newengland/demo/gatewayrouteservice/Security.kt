a750205a6d8933a6f3adb12da85b8cc9339f3f25
Chuck D'Antonio <c_dantonio@acm.org>
2019-01-14 14:38:10 -0500

    CSRF fix

--- src/main/kotlin/io/pivotal/pa/newengland/demo/gatewayrouteservice/Security.kt
+++ src/main/kotlin/io/pivotal/pa/newengland/demo/gatewayrouteservice/Security.kt
@@ -1,2 +1,18 @@
 package io.pivotal.pa.newengland.demo.gatewayrouteservice
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        http
+                .authorizeExchange().anyExchange().permitAll()
+                .and().csrf().disable()
+        return http.build()
+    }
+}
