a9cf732fce0134538c5e9b860f08d902993b449b
Chuck D'Antonio <c_dantonio@acm.org>
2019-01-14 14:36:54 -0500

    Add automatic service discovery with eureka

--- src/main/kotlin/io/pivotal/pa/newengland/demo/gatewayrouteservice/GatewayRouteServiceApplication.kt
+++ src/main/kotlin/io/pivotal/pa/newengland/demo/gatewayrouteservice/GatewayRouteServiceApplication.kt
@@ -1,11 +1,16 @@
 package io.pivotal.pa.newengland.demo.gatewayrouteservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [ SecurityAutoConfiguration::class ] )
+@EnableDiscoveryClient
+
 class GatewayRouteService
 
 fun main(args: Array<String>) {
 	runApplication<GatewayRouteService>(*args)
 }
+
