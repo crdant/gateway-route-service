4bda94201e822c5139a28a85a4e6367582c5fdcd
Chuck D'Antonio <c_dantonio@acm.org>
2019-02-05 15:22:27 -0500

    Removed outside rule

--- api-gateway/src/main/kotlin/io/pivotal/pa/newengland/demo/gateway/Routes.kt
+++ api-gateway/src/main/kotlin/io/pivotal/pa/newengland/demo/gateway/Routes.kt
@@ -8,8 +8,7 @@ import org.springframework.cloud.gateway.route.builder.routes
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.cloud.security.oauth2.gateway.TokenRelayGatewayFilterFactory
-
-
+import java.util.*
 
 
 @Configuration
@@ -17,15 +16,9 @@ class KotlinRoutes {
     @Autowired
     lateinit private var tokenRelay: TokenRelayGatewayFilterFactory
 
-
     @Bean("KotlinRoutes")
     fun routes(routeLocatorBuilder: RouteLocatorBuilder): RouteLocator =
             routeLocatorBuilder.routes {
-                route {
-                    path("/kotlin/**")
-                    filters { stripPrefix(1) }
-                    uri("http://httpbin.org")
-                }
                 route {
                     path("/portuguese")
                     uri( "lb://greeter")
