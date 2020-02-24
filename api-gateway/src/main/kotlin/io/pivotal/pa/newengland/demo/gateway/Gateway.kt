83208976b4ca9807307e06e1f7b5ea4a5a1b4f00
Chuck D'Antonio <cdantonio@pivotal.io>
2020-02-24 12:40:15 -0500

    Configure token relay if it's needed

--- api-gateway/src/main/kotlin/io/pivotal/pa/newengland/demo/gateway/Gateway.kt
+++ api-gateway/src/main/kotlin/io/pivotal/pa/newengland/demo/gateway/Gateway.kt
@@ -1,13 +1,17 @@
 package io.pivotal.pa.newengland.demo.gateway
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.security.oauth2.gateway.TokenRelayGatewayFilterFactory
 
 @SpringBootApplication
 @EnableDiscoveryClient
-
 class Gateway
+
+@Autowired
+private val filterFactory: TokenRelayGatewayFilterFactory? = null
 
 fun main(args: Array<String>) {
 	runApplication<Gateway>(*args)
