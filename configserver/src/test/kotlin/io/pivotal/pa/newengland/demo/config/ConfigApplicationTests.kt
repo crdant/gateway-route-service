aa3572a708cf8f0ef55e61ee5fa5942c122420ba
Chuck D'Antonio <cdantonio@pivotal.io>
2020-03-03 11:15:57 -0500

    Add Spring Cloud Config Server

---
+++ configserver/src/test/kotlin/io/pivotal/pa/newengland/demo/config/ConfigApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.pivotal.pa.newengland.demo.config
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
