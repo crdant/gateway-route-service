7cae853d79fc235e135d4a068b28b9d8a30fc038
Chuck D'Antonio <cdantonio@pivotal.io>
2020-03-04 14:10:16 -0500

    Further leverage multi-project build
    TL;DR
    -----
    
    Consolidate common elements into the top-level build and use Kotlin
    for all gradle files
    
    Details
    -------
    
    * Move common plugins to the top-level build script to simplify build
      management
    * Switch all build files to Kotlin syntax for consistency across all
      of the projects

---
+++ hello-service/build.gradle.kts
@@ -0,0 +1,28 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+  
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+	implementation("org.springframework.security:spring-security-oauth2-jose")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+  implementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
+
+  implementation("org.springframework.cloud:spring-cloud-starter-config")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+
+	testImplementation("org.springframework.security:spring-security-test")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+jib {
+  to.image = "crdant/worldly-hello"
+}
