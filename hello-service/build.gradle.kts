bf2be160936509e96859e3ddcfad99d370dad9ae
Chuck D'Antonio <cdantonio@pivotal.io>
2020-03-06 09:42:41 -0500

    Use `kpack` to build images
    TL;DR
    -----
    
    Build from source with `kpack`
    
    Details
    -------
    
    * Add a cluster builder to build the four spring boot services
      from source using Cloud Native Buildpacks
    * Migrate build script capabilities back into child projects so
      that the builder can build subprojects independently.

--- hello-service/build.gradle.kts
+++ hello-service/build.gradle.kts
@@ -1,20 +1,61 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-  
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+ 
+plugins {
+	id("org.springframework.boot") version "2.2.5.RELEASE" 
+	id("io.spring.dependency-management") version "1.0.9.RELEASE"
+	kotlin("jvm") version "1.3.61"
+	kotlin("plugin.spring") version "1.3.61"
+}
+
+repositories {
+  jcenter() 
+}
+
+group = "io.pivotal.pa.newengland.demo"
+version = "0.0.1-SNAPSHOT"
+
+configure<JavaPluginConvention> {
+  sourceCompatibility = JavaVersion.VERSION_11
+}
+
+extra["springCloudVersion"] = "Hoxton.SR1"
+
+tasks.withType<Test> {
+  useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+  kotlinOptions {
+    freeCompilerArgs = listOf("-Xjsr305=strict")
+    jvmTarget = "11"
+  }
+}
 
 dependencies {
-  implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-	implementation("org.springframework.security:spring-security-oauth2-jose")
-	implementation("org.springframework.boot:spring-boot-starter-security")
+  "implementation"("org.springframework.boot:spring-boot-starter-webflux")
+  "implementation"("org.springframework.boot:spring-boot-starter-actuator")
+  "implementation"("org.jetbrains.kotlin:kotlin-reflect")
+  "implementation"("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+  
+  "testImplementation"("org.springframework.boot:spring-boot-starter-test") {
+    exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+  }
+  "testImplementation"("io.projectreactor:reactor-test")
+}
 
-	implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
-  implementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
+dependencies {
+  "implementation"("org.springframework.boot:spring-boot-starter-oauth2-client")
+	"implementation"("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+	"implementation"("org.springframework.security:spring-security-oauth2-jose")
+	"implementation"("org.springframework.boot:spring-boot-starter-security")
 
-  implementation("org.springframework.cloud:spring-cloud-starter-config")
-	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+	"implementation"("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+  "implementation"("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
 
-	testImplementation("org.springframework.security:spring-security-test")
+  "implementation"("org.springframework.cloud:spring-cloud-starter-config")
+	"implementation"("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+
+	"testImplementation"("org.springframework.security:spring-security-test")
 }
 
 dependencyManagement {
@@ -23,6 +64,3 @@ dependencyManagement {
 	}
 }
 
-jib {
-  to.image = "crdant/worldly-hello"
-}
